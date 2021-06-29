37c7bda7d95eada882018b683955b3dace2d7bd5
Obi <mr.obiume@gmail.com>
2021-06-29 13:49:31 -0700

    commit tree 0a4e5619996e655a27df4486b77060815bfe92b5

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution in this file!
+// Code your solution in this file! 
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia']
+const returnFirstTwoDrivers = function() {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function() {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(x) {
+    return x * 4;
+}
+createFareMultiplier
